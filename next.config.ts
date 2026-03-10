9617c6ea6d659eade39acb12cb6170691e53b525
hobinyoo-acro <hb.yoo@acrofuture.com>
2026-03-10 15:44:46 +0900

    fix: remove rewrite

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: '/backend/:path*',
-        destination: 'http://15.164.221.121:8080/:path*',
-      },
-    ]
-  },
 }
 
 export default nextConfig
