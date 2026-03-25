1736d6678c629ad9ad590c64af402f21d509b124
hobinyoo-acro <hb.yoo@acrofuture.com>
2026-03-25 16:50:15 +0900

    fix: ssr ë prefetch 제거

--- src/app/debts/page.tsx
+++ src/app/debts/page.tsx
@@ -1,21 +1,5 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-
-import { getDebts } from '@/api/debt'
-import { DEBT_KEYS } from '@/queries/debt'
 import DebtTable from '@/components/debt/debt_table'
-import { getQueryClient } from '@/lib/query_client'
 
 export default async function DebtsPage() {
-  const queryClient = getQueryClient()
-
-  await queryClient.prefetchQuery({
-    queryKey: DEBT_KEYS.list(0),
-    queryFn: () => getDebts(0, 10),
-  })
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <DebtTable />
-    </HydrationBoundary>
-  )
+  return <DebtTable />
 }
--- src/app/investments/page.tsx
+++ src/app/investments/page.tsx
@@ -1,21 +1,5 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-
-import { getInvestments } from '@/api/investment'
-import { INVESTMENT_KEYS } from '@/queries/investment'
 import InvestmentTable from '@/components/investment/investment_table'
-import { getQueryClient } from '@/lib/query_client'
 
 export default async function InvestmentsPage() {
-  const queryClient = getQueryClient()
-
-  await queryClient.prefetchQuery({
-    queryKey: INVESTMENT_KEYS.list({ page: 0, size: 10 }), // 맞춰주기
-    queryFn: () => getInvestments({ page: 0, size: 10 }),
-  })
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <InvestmentTable />
-    </HydrationBoundary>
-  )
+  return <InvestmentTable />
 }
