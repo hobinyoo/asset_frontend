aa74da2808342d860c9b511078e95e067fb5ec01
hobinyoo-acro <hobinskyyy@naver.com>
2026-03-27 16:58:58 +0900

    fix: ssr -> csr 데일리리포트 수정

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,21 +1,5 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-
 import ReportPage from '@/components/report/report_page'
-import { getQueryClient } from '@/lib/query_client'
-import { REPORT_KEYS } from '@/queries/report'
-import { getReports } from '@/api/report'
 
 export default async function ReportsPage() {
-  const queryClient = getQueryClient()
-
-  await queryClient.prefetchQuery({
-    queryKey: REPORT_KEYS.list(),
-    queryFn: getReports,
-  })
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ReportPage />
-    </HydrationBoundary>
-  )
+  return <ReportPage />
 }
